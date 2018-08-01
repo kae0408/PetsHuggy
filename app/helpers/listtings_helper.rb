df5ac9a1c370b80e0f2e015f089e840695f577a3
kae0408 <k.tazawa.k@gmail.com>
2018-08-02 06:46:27 +0900

    listingページ

---
+++ app/helpers/listtings_helper.rb
@@ -0,0 +1,2 @@
+module ListtingsHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do  
+  resources :listings
+  
+
   root :to => 'pages#index'
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'registrations'}
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks",:registrations => 'registrations' }
 
   resources :users, only: [:show]
+
+  get 'manage-listing/:id/basics' => 'listings#basics', as: 'manage-listing_basics'
 end
